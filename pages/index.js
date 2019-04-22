548117ac63b9fb2a76ca83eb1e2122e6ccfc6f93
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2019-04-22 11:54:13 +0800

    update font with typekit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import Link from "next/link";
+import Head from "next/head";
 
 const Index = () => (
   <div>
+    <Head>
+      <link rel="stylesheet" href="https://use.typekit.net/xic5qkj.css" />
+    </Head>
+    <style jsx>{`
+      h1 {
+        font-family: brandon-grotesque, sans-serif;
+        font-weight: 400;
+        font-style: normal;
+      }
+    `}</style>
     <h1>SSR Magician</h1>
     <Link href="/about">
       <button>About</button>
