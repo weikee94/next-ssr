95cda1b73b46fd042bcd5787a9722c2934aba0af
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2019-05-06 10:48:26 +0800

    add medium article

--- pages/interest.js
+++ pages/interest.js
@@ -24,6 +24,10 @@ const Interest = () => {
           alt="roadmap"
         />
       </p>
+      <p>
+        to do
+        <a href="https://medium.com/@ju5td0m7m1nd/%E8%A8%98%E4%BD%8F%E9%80%99%E4%BA%94%E9%BB%9E-%E6%B2%92%E6%9C%89%E5%AD%B8%E4%B8%8D%E6%9C%83%E7%9A%84%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80-dce31e211479?fbclid=IwAR09aK2FkHkqTMPKPsmwvtTFipE5WLjr2dRNqzoxAxydrCaqVrU1MA1DhTo" />
+      </p>
     </div>
   );
 };
